b767ea18686b0d8a17fbfe7fcc4c3fcaefc61a43
Steven Zapata <szapata427@gmail.com>
2018-08-21 14:36:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,43 +1,56 @@
-var katzDeliLine = [];
+// function with two arguements
+// should return a welcome message including the new persons positions in line
+var num = 0
 
-function takeANumber(array, name) {
-  array.push(name);
-  return ("Welcome, " + name +". You are number " + (array.indexOf(name) + 1) + " in line.");
+function takeANumber(deline) {
+  num++
+  deline.push(num)
+  return ("Welcome. You are number " + num);
 }
 
-function nowServing(array) {
- var people = [];
- if (array.length < 1) {
- return "There is nobody waiting to be served!";
- }
- else { people.push(array[0]);
- array.shift();
- return "Currently serving " + people[0] + ".";
-   
- }
-}
+let katzDeli = []
 
+console.log(takeANumber(katzDeli))
+console.log(takeANumber(katzDeli))
+console.log(takeANumber(katzDeli))
+console.log(katzDeli)
 
-var first = [];
+// nowServing accepts the current line of people 
+// and returns the first person in line and then remove that individual from the line. 
+// if there is nobody on line, return "There is nobody waiting to be served."
+
+function nowServing(deline) {
+
   
-function currentLine(katzDeliLine) {
-  let i = 0;
-  while (i < katzDeliLine.length) {
-  first.push(` `+ [i+1] +`. `+katzDeliLine[i])
-  i++;
-  }
-  if (katzDeliLine.length === 0) {
-  return "The line is currently empty."
+  if ( 0 === deline.length) {
+    return "There is nobody waiting to be served!"
   }
   else {
-    return ("The line is currently:" + first)
+    return ("Currently serving " + deline.shift() + ".")
   }
 }
 
 
+// build a function "currentLine" accepts current line of people 
+// and returns the current line as a string (create a new variable)
+// if there is nobody in line, it return " The line is currently empty"
 
 
 
+function currentLine(deline) {
+  let i = 0
+  var line = [];
+  while (i < deline.length) {
+    line.push(" " + [i+1] + ". " + deline[i])
+    i++;
+  }
+  if ( 0 === deline.length) {
+    return "The line is currently empty."
+  }
+  else {
+    return ("The line is currently:" + line);
+  }
+}
 
 
 
